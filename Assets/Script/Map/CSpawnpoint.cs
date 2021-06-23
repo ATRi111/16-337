cde69247db5d059b157060ae94f797888228bfb1
ATRi111 <1657125180@qq.com>
2021-06-23 23:17:51 +0800

    fixed

--- Assets/Script/Map/CSpawnpoint.cs
+++ Assets/Script/Map/CSpawnpoint.cs
@@ -3,25 +3,8 @@ using UnityEngine;
 
 public class CSpawnpoint : MonoBehaviour
 {
-
-    private void OnEnable()
+    private void Awake()
     {
-        CEventSystem.Instance.AddLisenter<int>(EEventType.SceneLoad, Spawn);
-    }
-    private void OnDisable()
-    {
-        CEventSystem.Instance.RemoveListener<int>(EEventType.SceneLoad, Spawn);
-    }
-    public void Spawn(int index)
-    {
-        StartCoroutine(WaitForPlayer());
         CPlayer.Instance.transform.position = transform.position;
-    }
-    private IEnumerator WaitForPlayer()
-    {
-        for(float counter=0f;CPlayer.Instance == null && counter<1f; counter += Time.fixedDeltaTime)
-        {
-            yield return new WaitForFixedUpdate();
-        }
     }
 }
