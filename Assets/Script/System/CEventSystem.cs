2231c7d6f1fb98e61bf92ca139f54695f4e11dd0
ATRi111 <1657125180@qq.com>
2021-06-29 15:52:13 +0800

    new

--- Assets/Script/System/CEventSystem.cs
+++ Assets/Script/System/CEventSystem.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using Public;
 using System;
 
-public enum EEventType
+public enum EEvent
 {
     HPChange,
     PowerChange,
@@ -14,27 +14,27 @@ public enum EEventType
 
 public class CEventSystem : CSingleton<CEventSystem>
 {
-    private Dictionary<EEventType, Type> m_EventDict = new Dictionary<EEventType, Type>()
+    private Dictionary<EEvent, Type> m_EventDict = new Dictionary<EEvent, Type>()
     {
-        {EEventType.HPChange,typeof(Action<int,int>)},      //血量，血量上限
-        {EEventType.PowerChange,typeof(Action<int>)},       //火力
-        {EEventType.SpeedChange,typeof(Action<int>)},       //最大速度
-        {EEventType.Shoot,typeof(Action)},
+        {EEvent.HPChange,typeof(Action<int,int>)},      //血量，血量上限
+        {EEvent.PowerChange,typeof(Action<int>)},       //火力
+        {EEvent.SpeedChange,typeof(Action<int>)},       //最大速度
+        {EEvent.Shoot,typeof(Action)},
 
-        {EEventType.SceneLoad,typeof(Action<int>)},         //要加载的场景号
+        {EEvent.SceneLoad,typeof(Action<int>)},         //要加载的场景号
     };
-    private Dictionary<EEventType, Delegate> m_Event = new Dictionary<EEventType, Delegate>();
+    private Dictionary<EEvent, Delegate> m_Event = new Dictionary<EEvent, Delegate>();
 
     protected override void Awake()
     {
         base.Awake();
-        foreach (EEventType key in m_EventDict.Keys)
+        foreach (EEvent key in m_EventDict.Keys)
         {
             m_Event.Add(key, null);
         }
     }
 
-    private bool TypeCheck(EEventType eventType, Type methodType)
+    private bool TypeCheck(EEvent eventType, Type methodType)
     {
         if (m_EventDict[eventType] != methodType)
         {
@@ -44,49 +44,49 @@ public class CEventSystem : CSingleton<CEventSystem>
         return true;
     }
 
-    public void AddLisenter(EEventType eventType, Action listenerMethod)
+    public void AddLisenter(EEvent eventType, Action listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action)m_Event[eventType] + listenerMethod;
     }
-    public void AddLisenter<T1>(EEventType eventType, Action<T1> listenerMethod)
+    public void AddLisenter<T1>(EEvent eventType, Action<T1> listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action<T1>)m_Event[eventType] + listenerMethod;
     }
-    public void AddLisenter<T1, T2>(EEventType eventType, Action<T1, T2> listenerMethod)
+    public void AddLisenter<T1, T2>(EEvent eventType, Action<T1, T2> listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action<T1, T2>)m_Event[eventType] + listenerMethod;
     }
 
-    public void RemoveListener(EEventType eventType, Action listenerMethod)
+    public void RemoveListener(EEvent eventType, Action listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action)m_Event[eventType] - listenerMethod;
     }
-    public void RemoveListener<T1>(EEventType eventType, Action<T1> listenerMethod)
+    public void RemoveListener<T1>(EEvent eventType, Action<T1> listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action<T1>)m_Event[eventType] - listenerMethod;
     }
-    public void RemoveListener<T1, T2>(EEventType eventType, Action<T1, T2> listenerMethod)
+    public void RemoveListener<T1, T2>(EEvent eventType, Action<T1, T2> listenerMethod)
     {
         if (TypeCheck(eventType, listenerMethod.GetType()))
             m_Event[eventType] = (Action<T1, T2>)m_Event[eventType] - listenerMethod;
     }
 
-    public void ActivateEvent(EEventType eventType)
+    public void ActivateEvent(EEvent eventType)
     {
         if (TypeCheck(eventType, typeof(Action)))
             (m_Event[eventType] as Action)?.Invoke();
     }
-    public void ActivateEvent<T1>(EEventType eventType, T1 arg1)
+    public void ActivateEvent<T1>(EEvent eventType, T1 arg1)
     {
         if (TypeCheck(eventType, typeof(Action<T1>)))
             (m_Event[eventType] as Action<T1>)?.Invoke(arg1);
     }
-    public void ActivateEvent<T1, T2>(EEventType eventType, T1 arg1, T2 arg2)
+    public void ActivateEvent<T1, T2>(EEvent eventType, T1 arg1, T2 arg2)
     {
         if (TypeCheck(eventType, typeof(Action<T1, T2>)))
             (m_Event[eventType] as Action<T1, T2>)?.Invoke(arg1, arg2);
